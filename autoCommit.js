16359e9da04bd235b2ff6d5df689b832e14b5552
Umer-fareed21 <madfareed16@gmail.com>
2026-03-05 19:55:23 +0500

    commit 1

---
+++ autoCommit.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const { execSync } = require("child_process");
+
+const options = { stdio: "inherit", shell: "cmd.exe" };
+
+let totalCommits = 500;
+
+for (let i = 1; i <= totalCommits; i++) {
+  fs.appendFileSync("data.txt", "commit " + i + "\n");
+
+  execSync("git add .", options);
+  execSync(`git commit -m "commit ${i}"`, options);
+
+  console.log("commit " + i + " done");
+}
+
+console.log("all commits done");
